b5a1ef9bc1ece77d0fa1e74e8a13cc7da387af6b
Alexandra <sandra.larrosa88@gmail.com>
2020-06-30 08:37:05 +0200

    creation selectors js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 console.log('Hola caracola');
+
+//Selectores
+const inputNumber = document.querySelector('.js-inputNumber');
+const button = document.querySelector('.js-button');
+const textTrack = document.querySelector('.js-textTrack');
+const tryNumber = document.querySelector('.js-tryNumber');
+
+console.log(inputNumber);
+console.log(button);
+console.log(textTrack);
+console.log(tryNumber);
