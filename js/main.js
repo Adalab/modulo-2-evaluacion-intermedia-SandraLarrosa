891990781d34b7fdc23c72b45e8e3a2d2978a79e
Alexandra <sandra.larrosa88@gmail.com>
2020-06-30 10:02:11 +0200

    add disabled button and input

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-console.log('Hola caracola');
 
 //Selectores
 const inputNumber = document.querySelector('.js-inputNumber');
@@ -6,7 +5,8 @@ const button = document.querySelector('.js-button');
 const textTrack = document.querySelector('.js-textTrack');
 const tryNumber = document.querySelector('.js-tryNumber');
 const textWinner = document.querySelector('.js-textWinner');
-//Función que genera un número aleatorio
+
+
 
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
@@ -14,6 +14,7 @@ function getRandomNumber(max) {
 const randonNumber = getRandomNumber(100);
 
 console.log(`El número aleatorio generado es: ${randonNumber}`);
+
 
 let attemps = 0;
 
@@ -31,6 +32,8 @@ function checkNumber(ev) {
   if (randonNumber === numberValue) {
     textWinner.innerHTML = '¡Has ganado Campeona!';
     textWinner.classList.add('textWinner');
+    inputNumber.disabled = true;
+    button.disabled = true;
   } else {
     if (numberValue > 100 || numberValue < 0) {
       textTrack.innerHTML = 'El número debe estar entre el 1 y el 100';
