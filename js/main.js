185f6923f30411edfe29ada324151a03025ed566
Alexandra <sandra.larrosa88@gmail.com>
2020-06-30 09:22:28 +0200

    function check complete

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ const inputNumber = document.querySelector('.js-inputNumber');
 const button = document.querySelector('.js-button');
 const textTrack = document.querySelector('.js-textTrack');
 const tryNumber = document.querySelector('.js-tryNumber');
-
+const textWinner = document.querySelector('.js-textWinner');
 //Función que genera un número aleatorio
 
 function getRandomNumber(max) {
@@ -21,11 +21,24 @@ function checkNumber(ev) {
   ev.preventDefault();
 
   attemps++;
+  tryNumber.innerHTML = attemps;
 
   const numberValue = parseInt(inputNumber.value);
   console.log(
     `La usuaria ha introducido el número ${numberValue} en el intento ${attemps}`
   );
+
+  if (randonNumber === numberValue) {
+    textWinner.innerHTML = '¡Has ganado Campeona!';
+  } else {
+    if (numberValue > 100 || numberValue < 0) {
+      textTrack.innerHTML = 'El número debe estar entre el 1 y el 100';
+    } else if (numberValue < randonNumber) {
+      textTrack.innerHTML = 'Demasiado Bajo';
+    } else if (numberValue > randonNumber) {
+      textTrack.innerHTML = 'Demasiado Alto';
+    }
+  }
 }
 
 button.addEventListener('click', checkNumber);
